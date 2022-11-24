a39222f47e372fdbe27a03ba70eac1fd750652ce
kazim <kazim.mohammadi110@gmail.com>
2022-11-24 14:41:11 +0430

    Add js Linter

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-var hamburger=document.querySelector('.top-menu-list')
-var burgerMenu=document.querySelector('.nav-menu,.nav-menu-click')
-var burgerImg=document.querySelector('.nav-menu-img')
-var mainContainer=document.querySelector('.main-container')
-var works=document.querySelector('.works-section')
-var aboutme=document.querySelector('.aboutme-section')
-var contactme=document.querySelector('.contactme-section')
-var headline=document.querySelector('.headline')
-hamburger.style.display='flex'
+const hamburger = document.querySelector('.top-menu-list');
+const burgerMenu = document.querySelector('.nav-menu,.nav-menu-click');
+const burgerImg = document.querySelector('.nav-menu-img');
+const mainContainer = document.querySelector('.main-container');
+const menuIcon = document.querySelector('.icon');
+const menuList = document.querySelectorAll('.top-menu-list li');
 
 function hamburgerAction() {
-    if(hamburger.style.display==="block"){
-        hamburger.style.display="none";
-        burgerMenu.classList.replace('nav-menu-click','nav-menu')
-        burgerImg.src="img/icon-menu.png"
-        mainContainer.style.position='relative'
-    }else {
-        hamburger.style.display="block"
-        burgerMenu.classList.replace('nav-menu','nav-menu-click')
-        burgerImg.src="img/close.png"
-        mainContainer.style.position='fixed'
-    }
+  if (hamburger.style.display === 'block') {
+    hamburger.style.display = 'none';
+    burgerMenu.classList.replace('nav-menu-click', 'nav-menu');
+    burgerImg.src = 'img/icon-menu.png';
+    mainContainer.style.position = 'relative';
+  } else {
+    hamburger.style.display = 'block';
+    burgerMenu.classList.replace('nav-menu', 'nav-menu-click');
+    burgerImg.src = 'img/close.png';
+    mainContainer.style.position = 'fixed';
+  }
 }
 
-function scrollAction(){
-    if(hamburger.style.display==='block'){
-        mainContainer.style.position='relative'
-        hamburger.style.display="none";
-        burgerMenu.classList.replace('nav-menu-click','nav-menu')
-        burgerImg.src="img/icon-menu.png"
-    }
+function scrollAction() {
+  if (hamburger.style.display === 'block') {
+    mainContainer.style.position = 'relative';
+    hamburger.style.display = 'none';
+    burgerMenu.classList.replace('nav-menu-click', 'nav-menu');
+    burgerImg.src = 'img/icon-menu.png';
+  }
 }
+
+menuIcon.addEventListener('click', hamburgerAction);
+menuList.addEventListener('click', scrollAction);
